41122a51fcc6430485bfb655e473aa5a537e3947
gaurav-arun <grathore07@gmail.com>
2021-04-02 15:33:43 +0530

    Fix typo

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def index():
 
 def gen(camera):
     while True:
-        frame = camera.get_frame ()
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
